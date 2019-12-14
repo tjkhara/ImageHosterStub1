a1b9e3f3f63d94d3b276e7eb9806f42b92d5daf7
Tajeshwar Khara <tkhara@gmail.com>
2019-12-14 17:44:24 +0530

    Stub 1

---
+++ src/main/java/ImageHoster/controller/HomeController.java
@@ -0,0 +1,14 @@
+package ImageHoster.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String getHome() {
+        return "index";
+    }
+}
